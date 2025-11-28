496a2abe8043f1e96236b291091aaed671dd6637
Îømmi Mandela Fuentes <enum314@gmail.com>
2025-11-28 21:10:23 +0800

    test: add unit test for Xendit instance creation

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,1 +1,11 @@
 import { expect, test, describe } from "vitest";
+import { Xendit } from "../src";
+
+describe("Xendit", () => {
+  test("should create a new Xendit instance", () => {
+    const xendit = new Xendit("apiKey", "webhookSecret");
+
+    expect(xendit).toBeInstanceOf(Xendit);
+    expect(xendit.apiKey).toBe("apiKey");
+  });
+});
